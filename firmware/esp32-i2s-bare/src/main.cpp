e47beb7ecb022b0a9d6642527fd9f7d0d3004fed
andriy.malyshenko <andriy@sonocotta.com>
2025-09-05 22:53:54 +0200

    cleanup

--- firmware/esp32-i2s-bare/src/main.cpp
+++ firmware/esp32-i2s-bare/src/main.cpp
@@ -29,7 +29,7 @@ void setup()
   delay(500);
 #endif
 
-  esp_log_level_set("*", ESP_LOG_INFO);
+  esp_log_level_set("*", ESP_LOG_DEBUG);
 
   // initializes I2S, important!
   squareGenerator.init();
@@ -40,17 +40,31 @@ void setup()
   Tas5805m.init();
 
   // I2S must be initialized by this time for DSP settings to apply
-  uint8_t newVolume = 100;
+  uint8_t newVolume = 100; // 0...124, where 100 is max voltage swing without risk of clipping (assuming analog gain is set appropriately)
   ESP_LOGI(TAG, "Setting VOLUME to: %d%", newVolume);
   ESP_ERROR_CHECK(Tas5805m.setVolume100(newVolume));
 
-  ESP_LOGI(TAG, "Setting GAIN value to: -15.5Db");
-  ESP_ERROR_CHECK(Tas5805m.setAnalogGain(TAS5805M_MIN_GAIN));
-#endif
+  // If you power from onboard USB, you must set gain to minimum to avoid clipping
+  // ESP_LOGI(TAG, "Setting GAIN value to: -15.5dB");
+  // ESP_ERROR_CHECK(Tas5805m.setAnalogGain(TAS5805M_MIN_GAIN));
+
+  // If you power from external 15V+ supply, you can set gain to max
+  ESP_LOGI(TAG, "Setting GAIN value to: 0dB");
+  ESP_ERROR_CHECK(Tas5805m.setAnalogGain(TAS5805M_MAX_GAIN));
+  #endif
 
   cmd.init();
   cmd.registerCommandHandler(&toneCmd1);
   cmd.registerCommandHandler(&toneCmd2);
+
+  // Spin up tone automatically for demo purposes
+  // sineGenerator.setFrequency(100);
+  // sineGenerator.setAmplitude(14);
+  // sineGenerator.start();
+
+  // squareGenerator.setFrequency(1000);
+  // squareGenerator.setAmplitude(14);
+  // squareGenerator.start();
 
 }
 
