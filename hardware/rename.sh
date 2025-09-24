2befe64c29643c8f0cca401af9a8300a7705b02e
andriy.malyshenko <andriy@sonocotta.com>
2025-09-24 10:17:56 +0200

    Added HiFi-ESP32 hardware files

--- hardware/rename.sh
+++ hardware/rename.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-find . -type f -regex '.*/[A-Z][^/]*\.\(pdf\|zip\)' -print0 |
+find . -type f \( -iname '[A-Z]*.pdf' -o -iname '[A-Z]*.zip' -o -iname '[A-Z]*.step' \) -print0 |
 while IFS= read -r -d '' f; do
     dir=$(dirname "$f")
     base=$(basename "$f")
     new=$(echo "$base" \
         | sed -E 's/^([A-Za-z]+)_(.*)_([0-9]{4}-[0-9]{2}-[0-9]{2})(\..*)$/\2-\1\4/' \
         | tr '_' '-' \
-        | tr 'A-Z' 'a-z')
+        | tr '[:upper:]' '[:lower:]')
     if [ "$base" != "$new" ]; then
         mv "$f" "$dir/$new"
     fi
